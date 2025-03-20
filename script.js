7bc2410ecfddc4f3ad8289f7ee8d08a4172dce9b
Suresh Dike <sureshdike23@gmail.com>
2025-03-20 21:09:28 +0530

    Add files via upload
    first commit, uploading code for my Resume

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('toggleTheme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        this.textContent = '☀️ Light Mode';
+    } else {
+        this.textContent = '🌙 Dark Mode';
+    }
+});
